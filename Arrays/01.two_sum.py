b3b91a07491dcc029fb846bfed7aec8e642b380e
Dibyansh Mohanty <mohantydibyansh77@gmail.com>
2025-12-28 15:44:46 +0530

    updated two sum

--- Arrays/01.two_sum.py
+++ Arrays/01.two_sum.py
@@ -1,4 +1,10 @@
 '''
+
+LeetCode 1: Two Sum
+Approach: Hash Map
+Time Complexity: O(n)
+Space Complexity: O(n)
+
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 Example 1:
@@ -6,6 +12,7 @@ Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 '''
 
 def twoSum(nums, target):
